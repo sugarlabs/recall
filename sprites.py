f9acdc0952cf46e50905b0bd9f2f521aa9e35ef7
JuiP <juipradhan2k@gmail.com>
2020-04-19 19:49:28 +0530

    Run 2to3

--- sprites.py
+++ sprites.py
@@ -146,7 +146,7 @@ class Sprites:
         else:
             self.cr = cr
         if cr is None:
-            print 'sprites.redraw_sprites: no Cairo context'
+            print('sprites.redraw_sprites: no Cairo context')
             return
         for spr in self.list:
             if area == None:
@@ -268,7 +268,7 @@ class Sprite:
     def set_label(self, new_label, i=0):
         ''' Set the label drawn on the sprite '''
         self._extend_labels_array(i)
-        if type(new_label) is str or type(new_label) is unicode:
+        if type(new_label) is str or type(new_label) is str:
             # pango doesn't like nulls
             self.labels[i] = new_label.replace("\0", " ")
         else:
@@ -339,7 +339,7 @@ class Sprite:
     def draw(self, cr=None):
         ''' Draw the sprite (and label) '''
         if cr is None:
-            print 'sprite.draw: no Cairo context.'
+            print('sprite.draw: no Cairo context.')
             return
         for i, surface in enumerate(self.cached_surfaces):
             cr.set_source_surface(surface,
